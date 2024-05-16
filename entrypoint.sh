7f09106083fa719e9bde48fd372ebb354475fbe2
genoMichele <98103182+genoMichele@users.noreply.github.com>
2024-05-16 11:11:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,7 @@
 
 set -ue
 
-RepositoryName="${INPUT_REPOSITORY_NAME}"
-AwsRegion="${INPUT_AWS_REGION}"
-CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
+CodeCommitUrl="${INPUT_CODECOMMIT_URL}"
 
 git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
